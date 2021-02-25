4061b10e7f1b5931cf9bf5d95e37bd8ab77bd948
jimmoua1 <tsheejmaus@yahoo.com>
2021-02-24 16:46:00 -0800

    finalized test

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,33 @@
+const Accounts = require("../accounts/accounts-model")
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const { name, budget } = req.body;
+  if (name && budget) {
+    next();
+  } else {
+    res.status(400).json({ message: "Name and Budget required!" });
+  }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
+  const { name, id } = req.body;
+  const nameIDExist = await Accounts.getById(name, id);
+  if (nameIDExist){
+    next();
+  } else {
+    res.status(400).json({ message: `Unique name does not exist!` });
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  const { id } = req.params;
+  const idExist = await Accounts.getById(id);
+  if (idExist) {
+    next();
+  } else {
+    res.status(400).json({ message: `ID ${id} does not exist!` });
+  }
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,56 @@
-const router = require('express').Router()
+const express = require("express");
+const Account = require("./accounts-model");
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const grab = await Account.get();
+    res.status(200).json(grab);
+  } catch (e) {
+    next(e);
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const grabID = Account.getById(req.params.id);
+    res.status(200).json(grabID);
+  } catch (e) {
+    next(e);
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const postAccount = Account.post(req.body);
+    res.status(201).json(postAccount);
+  } catch (e) {
+    next(e);
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const { id } = req.params;
+    const edit = Account.put(id, req.body);
+    res.status(200).json(edit);
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const del = Account.remove(req.params.id);
+    res.status(200).json(del);
+  } catch (e) {
+    next(e);
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
